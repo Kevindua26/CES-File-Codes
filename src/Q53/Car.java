18d9c5983985b1518d3c7ed6e9194b6e9ff9d286
Kaivalaya Dua <kevindua26@gmail.com>
2024-10-28 21:48:15 +0530

    Added Solution of Q53

---
+++ src/Q53/Car.java
@@ -0,0 +1,18 @@
+package Q53;
+
+public class Car {
+    // Attributes
+    String color;
+    int year;
+
+    // Constructor
+    public Car(String c, int y) {
+        color = c;
+        year = y;
+    }
+
+    // Method
+    public void start() {
+        System.out.println("The car is starting.");
+    }
+}

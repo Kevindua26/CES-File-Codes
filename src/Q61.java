6d63cc0bb19ee5333d0aa5594e484135bacb0cbc
Kaivalaya Dua <kevindua26@gmail.com>
2024-11-02 16:04:53 +0530

    Added Solution of Q61

---
+++ src/Q61.java
@@ -0,0 +1,27 @@
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+class Puppy extends Dog {
+    void weep() {
+        System.out.println("The puppy weeps.");
+    }
+}
+
+public class Q61 {
+    //Write a java program to demonstrate multilevel inheritance.
+    public static void main(String[] args) {
+        Puppy puppy = new Puppy();
+        puppy.eat(); // Inherited from Animal
+        puppy.bark(); // Inherited from Dog
+        puppy.weep(); // Method in Puppy
+    }
+}
